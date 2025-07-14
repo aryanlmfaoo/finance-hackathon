6ac3b0fc0e86058b7ebc53b1b5f86ce45cb74bac
aryan sharma <aryansharm1223@gmail.com>
2025-07-14 22:59:33 +0530

    basic setup, has a quiz page in the frontend

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Page() {
+  return (
+    <View className="flex flex-1 justify-center items-center">
+      <Text className="">Hello World</Text>
+      <Link href="/quiz">Quiz Page</Link>
+    </View>
+  );
+}
+
